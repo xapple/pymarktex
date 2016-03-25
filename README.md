d454f7eb45b37f342d2dbd2c356efcb7b3e0b782
Lucas Sinclair <lucas.sinclair@me.com>
2016-03-25 17:39:46 +0100

    Update package to standards

--- README.md
+++ README.md
@@ -1,4 +1,6 @@
-## pymarktex
+[![changelog](http://allmychanges.com/p/python/pymarktex/badge/)](http://allmychanges.com/p/python/pymarktex/?utm_source=badge) [![PyPI version](https://badge.fury.io/py/pymarktex.svg)](https://badge.fury.io/py/pymarktex)
+
+# `pymarktex` version 1.0.2
 
 This program creates a PDF file out of a specifically formated markdown text file.
 In particular, the markdown file can contain extra information that
@@ -21,4 +23,9 @@ And it will generate the corresponding `lorem.pdf` file in the same directory.
     $ tar xzf install-tl-unx.tar.gz
     $ cd install-tl-20141218
     $ ./install-tl -gui text
-    Change the install directory to something like: ~/programs/textlive/2014
+
+Then, change the install directory to something like: `~/programs/textlive/2014`
+
+### Code documentation
+More documentation is available at:
+http://xapple.github.io/pymarktex/
--- setup.py
+++ setup.py
@@ -1,15 +1,15 @@
 from distutils.core import setup
 
-with open('README.md') as readme: long_desc = readme.read()
-
 setup(
-        name             = 'pymarktex',
-        version          = '1.0.2',
-        description      = 'Will convert a markdown text file to a fancy PDF document',
-        long_description = long_desc,
-        license          = 'MIT',
-        url              = 'http://github.com/xapple/pymarktex/',
-        author           = 'Lucas Sinclair',
-        author_email     = 'lucas.sinclair@me.com',
-        install_requires = ['sh', 'pystache'],
-    )
+    name             = 'pymarktex',
+    version          = '1.0.2',
+    description      = 'Will convert a markdown text file to a fancy PDF document',
+    license          = 'MIT',
+    url              = 'http://github.com/xapple/pymarktex/',
+    author           = 'Lucas Sinclair',
+    author_email     = 'lucas.sinclair@me.com',
+    packages         = ['pymarktex'],
+    scripts          = ['scripts/pymarktex'],
+    install_requires = ['sh', 'pystache'],
+    long_description = open('README.md').read(),
+)
