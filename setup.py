6d7f609ce9b554c6bc615e846c7d27e7fb76338d
Lucas Sinclair <649288+xapple@users.noreply.github.com>
2019-02-21 16:32:00 +0100

    Version 1.0.7

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name             = 'pymarktex',
-    version          = '1.0.6',
+    version          = '1.0.7',
     description      = 'Will convert a markdown text file to a fancy PDF document',
     license          = 'MIT',
     url              = 'http://github.com/xapple/pymarktex/',
