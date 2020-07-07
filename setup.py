65fa0c9d73aabf418eb280bf3264e36e0b39401e
Lucas Sinclair <649288+xapple@users.noreply.github.com>
2020-07-07 23:11:35 +0200

    Update dependency version numbers

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ setup(
     author_email     = 'lucas.sinclair@me.com',
     packages         = find_packages(),
     scripts          = ['scripts/pymarktex'],
-    install_requires = ['plumbing>=2.8.0', 'autopaths>=1.4.1', 'pystache>=0.5.4'],
+    install_requires = ['plumbing>=2.8.1', 'autopaths>=1.4.2', 'pystache>=0.5.4'],
     long_description = open('README.md').read(),
     long_description_content_type = 'text/markdown',
     include_package_data = True,
