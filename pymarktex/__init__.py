610cf0b86d743f3bab418d1e4ea4ae9d64d6b01a
Lucas Sinclair <649288+xapple@users.noreply.github.com>
2020-07-20 14:01:33 +0200

    Back to development version.

--- pymarktex/__init__.py
+++ pymarktex/__init__.py
@@ -2,7 +2,7 @@
 # -*- coding: utf-8 -*-
 
 # Special variables #
-__version__ = '1.4.4'
+__version__ = '1.4.5'
 
 # Built-in modules #
 import os, sys, re, shutil, codecs, importlib
--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name             = 'pymarktex',
-    version          = '1.4.4',
+    version          = '1.4.5',
     description      = 'Will convert a markdown text file to a fancy PDF document',
     license          = 'MIT',
     url              = 'http://github.com/xapple/pymarktex/',
