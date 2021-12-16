0c29423a214d02f3fc69f1352eaa5621a714b0bf
Lucas Sinclair <649288+xapple@users.noreply.github.com>
2021-12-16 22:06:32 +0100

    Attempt to fix the "old_school" template

--- pymarktex/templates/old_school/__init__.py
+++ pymarktex/templates/old_school/__init__.py
@@ -9,8 +9,17 @@ class HeaderTemplate(Template):
     def company(self):     return self.options.get('company')
     def subcompany(self):  return self.options.get('subcompany')
     def title(self):       return self.options.get('title')
-    def image_left(self):  return self.options.get('image_left').unix_style
-    def image_right(self): return self.options.get('image_right').unix_style
+
+    def image_left(self): return self.include_image('image_left')
+
+    def image_right(self): return self.include_image('image_right')
+
+    img_template = "\includegraphics[width=11mm, height=11mm]{%s}}"
+
+    def include_image(self, name):
+        result = self.options.get('image_right')
+        if result is None: return ""
+        return img_template % name.unix_style
 
 ###############################################################################
 class FooterTemplate(Template):
