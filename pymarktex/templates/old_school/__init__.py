636f6b96b26dea3fe9567d62e4dee375c5841d37
Lucas Sinclair <649288+xapple@users.noreply.github.com>
2021-12-16 22:17:47 +0100

    Fixed the "old_school" template

--- pymarktex/templates/old_school/__init__.py
+++ pymarktex/templates/old_school/__init__.py
@@ -1,25 +1,32 @@
 # Internal modules #
+from pymarktex import logo_dir
 from pymarktex.templates import Template
+
+# First party modules #
+from autopaths.file_path import FilePath
 
 ###############################################################################
 class HeaderTemplate(Template):
     """All the parameters to be rendered in the LaTeX header template"""
-    def name(self):        return self.options.get('name')
-    def status(self):      return self.options.get('status')
-    def company(self):     return self.options.get('company')
-    def subcompany(self):  return self.options.get('subcompany')
-    def title(self):       return self.options.get('title')
+    def name(self):        return self.option_or_empty('name')
+    def status(self):      return self.option_or_empty('status', ', ')
+    def company(self):     return self.option_or_empty('company')
+    def subcompany(self):  return self.option_or_empty('subcompany')
+    def title(self):       return self.option_or_empty('title')
 
-    def image_left(self): return self.include_image('image_left')
-
-    def image_right(self): return self.include_image('image_right')
-
-    img_template = "\includegraphics[width=11mm, height=11mm]{%s}}"
-
-    def include_image(self, name):
-        result = self.options.get('image_right')
+    def option_or_empty(self, name, prefix=None):
+        result = self.options.get(name)
         if result is None: return ""
-        return img_template % name.unix_style
+        if prefix is not None: return prefix + result
+        return result
+
+    def image_path(self):
+        # Get the option #
+        result = self.options.get('image_path')
+        # If it's a path #
+        if isinstance(result, FilePath): return result.unix_style
+        # If it's a string #
+        if isinstance(result, str): return (logo_dir + result).unix_style
 
 ###############################################################################
 class FooterTemplate(Template):
